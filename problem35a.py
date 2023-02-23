63e944d25f4ad0e5dc76d752ccbeecaebd491345
Michalous <49964028+Michalous@users.noreply.github.com>
2023-02-23 23:23:39 +0000

    problem35 much faster solution added

---
+++ problem35a.py
@@ -0,0 +1,30 @@
+from problem7 import sieveOfEratosthenes
+
+prim = sieveOfEratosthenes(1000000)
+primes = set(prim)
+bad_digits = [2, 4, 5, 6, 8, 0]
+result = 2 # for 2 and 5 that are in bad_digits
+
+not_primes = set()
+
+for num in primes:
+    for i in range(len(str(num))):
+        if int(str(num)[i]) in bad_digits:
+            #primes.remove(num)
+            not_primes.add(num)
+            break 
+
+primes -= not_primes
+for x in primes:
+	count = 0
+	p = x
+	for i in range(len(str(x))):
+		p = str(p)[-1] + str(p)[:-1]
+		if int(p) not in primes:
+			break
+			
+		count += 1
+	if count == len(str(x)):
+		result += 1
+		print(x)
+print(f'Result: {result}')
